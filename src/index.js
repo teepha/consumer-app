ebf3043f27ed698f6513ec3e68c9c251be6dc717
Lateefat Amuda <teepha06@gmail.com>
2020-02-22 21:53:55 +0100

    - add catch for db connection error

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import connectToDB from "./config/db";
 const app = express();
 const port = config.port;
 
-connectToDB().then(() => {
-  console.log("Connected to DB...");
-  app.listen(port, () => {
-    console.log(`Listening on port ${port}...`);
-  });
-});
+connectToDB()
+  .then(() => {
+    console.log("Connected to DB...");
+    app.listen(port, () => {
+      console.log(`Listening on port ${port}...`);
+    });
+  })
+  .catch(error => console.log("An error occured. Unable to start server."));
 
 processJob();
